8cb8e6984e352a831da674fc06a44df09ca278aa
Wenny Hidayat <wenny.h@live.com>
2019-12-15 22:01:14 +1100

    Init empty widget

--- lib/cdk-serverless-stack.ts
+++ lib/cdk-serverless-stack.ts
@@ -1,9 +1,11 @@
 import cdk = require('@aws-cdk/core');
+import widget_service = require('../lib/widget_service');
 
 export class CdkServerlessStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    new widget_service.WidgetService(this, 'Widgets');
   }
 }
